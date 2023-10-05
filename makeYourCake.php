0fbc9e821a2687c4b22dddeb1a4b36ee0b474b5e
Happy2902 <2020.harpreetsingh.kataria@vws.ac.in>
2023-10-05 21:42:13 +0530

    New Project

---
+++ makeYourCake.php
@@ -0,0 +1,54 @@
+<?php 
+    define('Access', TRUE);
+    include "./AdditionalPHP/startSession.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en-MU">
+    <head>
+        <meta charset="utf-8">
+        <title>PATISSERIE | MAKE YOUR CAKE</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--CSS File-->
+        <link rel="stylesheet" type="text/css" href="Common.css">
+        <link rel="stylesheet" type="text/css" href="random.css">
+        <!-- Font Awesome -->
+        <script src="https://kit.fontawesome.com/0e16635bd7.js" crossorigin="anonymous"></script>
+        <!--BOXICONS-->
+        <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+        <!-- Animate CSS -->
+        <link rel="stylesheet"href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+    </head>
+
+    <body>
+        <?php $page = 'makeyourcake';?>
+
+        <!--Start Navigation Bar-->
+        <?php include './Includes/MobileNavBar.php';?>
+        <!--End Navigation Bar-->
+
+
+        <!--Start Navigation Bar @media 1200px-->
+        <?php include './Includes/PcNavBar.php';?>
+        <!--End Navigation Bar @media 1200px-->
+
+        
+        <!--Start Wave Image-->
+        <div class="wave-image-group">
+            <div class="wave-image footer-wave">
+                <img src="Assets/images/1.index/NavBar_WavePink.png">
+            </div>
+        </div>
+        <!--End Wave Image-->
+
+
+        <!--Start Footer-->
+        <?php include './Includes/Footer.php';?>
+        <!--End Footer-->
+
+        
+        <!-- Start Bottom Nav -->
+        <?php include './Includes/MobileBottomNav.php';?>
+        <!-- End Bottom Nav -->
+    </body>
+</html>
